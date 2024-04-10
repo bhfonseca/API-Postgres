d44ee6f7bc7a687d5af54757c6f1c9ff89160933
bhfonseca <bruno_h_fonseca@outlook.com.br>
2024-04-09 21:11:23 -0300

    commit

--- models/delete.go
+++ models/delete.go
@@ -2,14 +2,14 @@ package models
 
 import "api-postgesql/db"
 
-func Update(id int64, todo Todo) (int64, error) {
+func Delete(id int64, todo Todo) (int64, error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return 0, err
 	}
 	defer conn.Close()
 
-	res, err := conn.Exec(`update todos set title=$1, description=$2, done=$3 where id=$4`, todo.Title, todo.Description, todo.Done, id)
+	res, err := conn.Exec(`DELETE FROM todos where id=$1`, id)
 	if err != nil {
 		return 0, err
 	}
